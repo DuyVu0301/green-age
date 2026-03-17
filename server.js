cdc6b7165e9f458341135d9a6e653443e2574ec8
Le Duy Vu <leduyvupro123@gmail.com>
2026-03-17 13:55:25 +0700

    update

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.get("/", (req, res) => res.render("index"));
+app.listen(3000, () => console.log("Server chạy tại port 3000"));
